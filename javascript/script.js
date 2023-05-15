9d00d89ad20631179fa9fd2ccb813694c46c3714
Anabela Harych <anabelaharych@outlook.com>
2023-05-15 09:10:24 -0300

    Proyecto Web

--- javascript/script.js
+++ javascript/script.js
@@ -3,24 +3,31 @@ function calcularPrecioTotal() {
     console.log("ingreso a calcularPrecioTotal");
 
     //en cantEnt almaceno la cant de entradas que ingresó el usuario
-    let cantEnt=document.getElementById("cantEntradas").value;
+    let cantEnt = document.getElementById("cantEntradas").value;
     console.log("cantEnt: " + cantEnt);
 
-    document.getElementById("titulo").innerHTML="Comprar " + cantEnt + " entradas."
+    document.getElementById("titulo").innerHTML = "Comprar " + cantEnt + " entradas."
 
     //en porcDesc almaceno el descuento que seleccionó el usuario
-    let porcDesc=document.getElementById("porcDescuento").value;
+    let porcDesc = document.getElementById("porcDescuento").value;
     console.log("porcDesc: " + porcDesc);
 
     //en descuento almacenamos el total de pesos para descontar
-    let descuento=(cantEnt*5000) * porcDesc/100;
+    let descuento = (cantEnt * 5000) * porcDesc / 100;
     console.log("descuento: " + descuento);
 
     //pretio total a pagar con el descuento
-    let precioFin=(cantEnt*5000) - descuento;
+    let precioFin = (cantEnt * 5000) - descuento;
     console.log("precioFin: " + precioFin);
 
     //modifico el input precio final para poner el total con descuento
-    document.getElementById("precioFinal").value=precioFin;
-    document.getElementById("precionFinal2").innerHTML=precioFin;
+    document.getElementById("precioFinal").value = descuento;
+    document.getElementById("precionFinal2").innerHTML = precioFin;
 }
+
+$(document).ready(function () {
+    $('#btn1').click(function () {
+        $('#modal').modal('show');
+        $('#modal').draggable();
+    });
+});
